add8a488bf6952aa1e4ae3b71c727a988d6d67d2
Toshiki Togo <toshiki0221lol@gmail.com>
2025-11-14 17:30:23 +0900

    feat(typical90): Review 004
    ## 🔄 Review (004)
    - Re-implemented from scratch.

--- typical90_d/main.py
+++ typical90_d/main.py
@@ -1,30 +1,28 @@
 import sys
 
-#H,Wを受け取る
-h,w = map(int,sys.stdin.readline().split())
+#高速入力の準備
+input = sys.stdin.readline
 
-#マス目の整数を受け取る
-num_map = []
+#1. H,Wを受け取る
+h, w = map(int, input().split())
 
+#2. マップを受け取る
+num_map = [list(map(int, input().split())) for _ in range(h)]
+
+#3. 前計算(ここがPythonicポイント!)
+#行の和: 各行(row)に対して sum() を適用
+w_sum = list(map(sum, num_map))
+
+#列の和: zip(*num_map)で行列を「転置(縦横入れ替え)」してからsum() を適用
+# *num_map でリストを展開し、zipで同じインデックスの要素をまとめる
+h_sum = list(map(sum, zip(*num_map)))
+
+#4. 答えを計算し出力する(ここは同じ)
 for i in range(h):
-    s = list(map(int,input().split())) #sys.stdin.readline()使いたかった；；
-    num_map.append(s)
-
-#各行の和、各列の和を計算する
-w_sum = [0] * h
-h_sum = [0] * w
-
-for i in range(h):
+    answer_line = []
     for j in range(w):
-        w_sum[i] += num_map[i][j]
-        h_sum[j] += num_map[i][j]
+        #w_sum[i] + h_sum[j] - num_map[i][j]
+        answer_line.append(str(w_sum[i] + h_sum[j] - num_map[i][j]))
 
-#結果を計算して１行ごとに出力する
-
-for i in range(h):
-    s = []
-    for j in range(w):
-        a = w_sum[i] + h_sum[j] - num_map[i][j]
-        s.append(str(a))
-
-    print(" ".join(s))
+    print(" ".join(answer_line))
+    
